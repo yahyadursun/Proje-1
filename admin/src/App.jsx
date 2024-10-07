4adfd063c89eab32e06c802129276c81f83939db
yahyadursun <114141165+yahyadursun@users.noreply.github.com>
2024-10-07 18:18:44 +0300

    Admin panel listing
    completed the admin panel features Listing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -12,6 +12,8 @@ import "react-toastify/dist/ReactToastify.css";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
+export const currency ="₺";
+
 const App = () => {
   const [token, setToken] = useState(
     localStorage.getItem("token") ? localStorage.getItem("token") : ""
