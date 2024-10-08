e06a2387e77cf03aa704bea261408f76b8115bdb
Tuğberk Erin <tugberkerin2002@hotmail.com>
2024-10-08 19:16:55 +0300

    routes&pages
    assetler lazım asset olmadan home about vs yerine oturmamış halde duruyor assets ekleyip navbar <img src={assets.logo} className="w-36" alt="" /> yerleştirilmeli

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+      
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>HOME</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1">
+          <p>COLLECTION</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>ABOUT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>CONTACT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
