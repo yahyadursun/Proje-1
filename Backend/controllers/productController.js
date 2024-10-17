52cd98b8cdc5a2f12fc4ac36250df4d84096703f
yahyadursun <114141165+yahyadursun@users.noreply.github.com>
2024-10-17 18:57:30 +0300

    backend update
    In this commit I added brand feature

---
+++ Backend/controllers/productController.js
@@ -0,0 +1,94 @@
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModels.js";
+
+// function for add product
+const addProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      brand,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller,
+    } = req.body;
+    // for an error about trying to read not existing image
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
+    //taking url for images
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
+
+    const productData = {
+      name,
+      description,
+      brand,
+      price: Number(price),
+      category,
+      subCategory,
+      sizes: JSON.parse(sizes), // need to config for what we selling? // parsing for array
+      bestseller: bestseller === "true" ? true : false,
+      image: imagesUrl,
+      date: Date.now(),
+    };
+    console.log(productData);
+
+    const product = new productModel(productData);
+    await product.save();
+
+    res.json({ success: true, message: "Product added successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, error: error.message });
+  }
+};
+
+// function for add list product
+const listProduct = async (req, res) => {
+    try {
+        const products = await productModel.find({});
+        res.json({success:true,products})
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, error: error.message });
+    }
+};
+
+// function for add removing product
+const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Product removed successfully" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, error: error.message });
+    }
+};
+
+// function for add single product info
+const singleProduct = async (req, res) => {
+    try {
+        const {productId} = req.body;
+        const product = await productModel.findById(productId);
+        res.json({success:true,product})
+    } catch (error) {  
+        console.log(error);
+        res.json({ success: false, error: error.message });
+    }
+};
+
+export { listProduct, removeProduct, singleProduct, addProduct };
---
+++ Backend/models/productModels.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, require: true },
+  description: { type: String, require: true },
+  brand: {type: String, require: true },
+  price: { type: Number, require: true },
+  image: { type: Array, require: true },
+  category: { type: String, require: true },
+  subCategory: { type: String, require: true },
+  sizes: { type: Array, require: true },
+  bestseller: { type: Boolean },
+  date: { type: Number, require: true },
+});
+
+const productModel =
+  mongoose.models.product || mongoose.model("product", productSchema);
+
+export default productModel;
