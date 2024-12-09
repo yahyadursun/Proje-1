bf5df32d7e5ea8914178b950a7f28e3f321c02d2
Tuğberk Erin <tugberkerin2002@hotmail.com>
2024-12-09 21:43:37 +0300

    filterv1.6
    renk yok gerisi çok

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -19,21 +19,21 @@ const Collection = () => {
   const modalRef = useRef(null);
 
   const categories = [
-    { id: "category", label: "Cinsiyet", options: ["Men", "Women", "Unisex"] },
+    { id: "category", label: "Cinsiyet", options: ["Erkek", "Kadın", "Unisex","Kids"] },
     {
       id: "subCategory",
-      label: "Ayakkabı Türü",
-      options: ["Daily", "Basketball", "Klasik", "Outdoor", "Others"],
+      label: "Kategori",
+      options: ["Bot","Koşu Ayakkabısı", "Outdoor", "Sneaker","Sandalet","Terlik"],
     },
     {
       id: "sizes",
-      label: "Ayakkabı Numarası",
-      options: [36, 37, 38, 39, 40],
+      label: "Beden",
+      options: [0, 37, 38, 39, 40],
     },
     {
       id: "brand",
       label: "Marka",
-      options: ["New Balance", "Adidas", "Puma", "Nike"],
+      options: ["New Balance", "Adidas", "Puma", "Nike","Reebok","The North Face","Timberland","Tommy Hilfiger","Vans","Jack Wolfskin","U.S. Polo Assn."],
     },
     {
       id: "color",
@@ -122,14 +122,14 @@ const Collection = () => {
         <div className="flex items-center gap-4">
           <button
             onClick={() => setShowFilterModal(true)}
-            className="flex items-center px-4 py-2 text-sm bg-gradient-to-r from-white to-gray-200 text-black rounded-full shadow-lg hover:opacity-90 transition-all duration-300"
+            className="flex funnel-sans items-center px-4 py-2 text-sm bg-gradient-to-r from-white to-gray-200 text-black rounded-full shadow-lg hover:opacity-90 transition-all duration-300"
           >
             Filtreler
           </button>
           <select
             value={sortType}
             onChange={(e) => setSortType(e.target.value)}
-            className="px-4 py-2 text-sm border rounded-full shadow-md bg-gradient-to-r from-white to-gray-200"
+            className="px-4 funnel-sans py-2 text-sm border rounded-full shadow-md bg-gradient-to-r from-white to-gray-200"
           >
             <option value="relevant">Sırala</option>
             <option value="price-asc">Fiyat: Azdan Çoğa</option>
@@ -158,10 +158,10 @@ const Collection = () => {
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-end z-50">
           <div
             ref={modalRef}
-            className="bg-white w-full max-w-lg h-full p-6 flex flex-col divide-y divide-gray-200"
+            className="bg-white w-full  funnel-sans max-w-lg h-full p-6 flex flex-col divide-y divide-gray-200"
           >
-            <div className="flex justify-between items-center py-4">
-              <h2 className="text-2xl text-gray-900">FİLTRELER</h2>
+            <div className="flex funnel-sans justify-between items-center py-4">
+              <h2 className="text-2xl funnel-sans text-gray-900">FİLTRELER</h2>
               <div className="flex gap-6">
                 <button
                   onClick={clearFilters}
@@ -171,19 +171,19 @@ const Collection = () => {
                 </button>
                 <button
                   onClick={() => setShowFilterModal(false)}
-                  className="text-2xl font-bold text-gray-500 hover:text-black"
+                  className="text-2xl funnel-sans font-bold text-gray-500 hover:text-black"
                 >
                   ✕
                 </button>
               </div>
             </div>
-            <div className="flex flex-grow">
-              <div className="w-2/4 border-r border-gray-200 p-4">
+            <div className="flex funnel-sans flex-grow">
+              <div className="w-2/4 funnel-sans border-r border-gray-200 p-4">
                 {categories.map((cat) => (
                   <button
                     key={cat.id}
                     onClick={() => setSelectedCategory(cat)}
-                    className={`w-full text-left py-3 px-6 text-lg font-medium ${
+                    className={`w-full funnel-sans text-left py-3 px-6 text-lg font-medium ${
                       selectedCategory?.id === cat.id
                         ? "bg-gray-200 text-gray-900"
                         : "hover:bg-gray-100 text-gray-600"
@@ -196,12 +196,12 @@ const Collection = () => {
               <div className="w-2/3 p-4">
                 {selectedCategory &&
                   selectedCategory.options.map((option) => (
-                    <label key={option} className="flex items-center gap-3 py-3">
+                    <label key={option} className="funnel-sans flex items-center gap-3 py-3">
                       <input
                         type="checkbox"
                         checked={filters[selectedCategory.id]?.includes(option)}
                         onChange={() => toggleFilter(selectedCategory.id, option)}
-                        className="w-5 h-5"
+                        className="w-4 h-4 "
                       />
                       {option}
                     </label>
@@ -213,7 +213,7 @@ const Collection = () => {
                 applyFilter();
                 setShowFilterModal(false);
               }}
-              className="w-full py-3 bg-black text-white text-xl rounded-md"
+              className="w-full funnel-sans py-3 bg-black text-white text-xl rounded-md"
             >
               Filtreleri Uygula
             </button>
