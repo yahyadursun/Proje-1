e089b1b78f9cdf3b8cff14612c80dadde6c7801f
yahyadursun <114141165+yahyadursun@users.noreply.github.com>
2025-01-02 15:43:45 +0300

    hot fix admin panel

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,4 +1,5 @@
 import logo from './logo.png'
+import logo1 from './logo1.jpg'
 import add_logo from './add_logo.jpg'
 import order_logo from './order_logo.png'
 import list_logo from './list_logo.png'
@@ -7,6 +8,7 @@ import parcel_icon from './parcel_icon.png'
 
 export const assets = {
     logo,
+    logo1,
     add_logo,
     list_logo,
     order_logo,
