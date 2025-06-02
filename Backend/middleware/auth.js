d93a69f7e47c89ae63d752383cbee7216ceacb22
yahyadursun <114141165+yahyadursun@users.noreply.github.com>
2025-06-02 23:59:02 +0300

    adding readme

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,12 +1,9 @@
 import jwt from 'jsonwebtoken'
-
 const authUser = async (req,res,next)=> {
     const {token} = req.headers;
-
     if (!token) {
         return res.json({ success:false, message: 'Not Authorized Login Again'})    
     }
-
     try {
 
         const token_decode= jwt.verify(token,process.env.JWT_SECRET)
@@ -18,5 +15,4 @@ const authUser = async (req,res,next)=> {
         res.json({ success:false, message: error.message })
     }
 }
-
 export default authUser
